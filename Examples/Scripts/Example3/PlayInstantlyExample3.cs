ac106fc995cd2994c294c14f8d8008a021993a21
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-03-30 17:41:32 +0200

    Play instant example

---
+++ Examples/Scripts/Example3/PlayInstantlyExample3.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Juce.TweenComponent.Example3
+{
+    public class PlayInstantlyExample3 : MonoBehaviour
+    {
+        [SerializeField] private TweenPlayer tweenPlayer = default;
+
+        private void Start()
+        {
+            tweenPlayer.Play(instantly: true);
+
+            tweenPlayer.Play(instantly: false);
+        }
+    }
+}
